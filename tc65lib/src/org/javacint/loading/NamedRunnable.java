f5394a8dac38e48d3582fb0ca91cc017e4f7c3be
Florent Clairamault <florent@clairambault.fr>
2013-05-19 18:40:32 +0200

    Namespace change.

---
+++ tc65lib/src/org/javacint/loading/NamedRunnable.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javacint.loading;
+
+/**
+ * Named runnabled. Should be used to identify different startup tasks.
+ *
+ 
+ */
+public abstract class NamedRunnable {
+
+	private final String name;
+
+	public NamedRunnable() {
+		this.name = "!!! DEFINE A NAME !!!";
+	}
+
+	public NamedRunnable(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public abstract void run() throws Exception;
+}
