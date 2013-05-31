31d27d2629e6c266ef6f9bcbce9d991dca0d7bb3
Nikita Kapitonov <nikita.kapitonov@gmail.com>
2013-06-01 00:33:53 +0400

    minor improve: 	SMSReceiver 	- fixed excess synchronized issue, added some comments MAJOR improve: 	ATCommands 	- fixed ATCommand excessive constructor issue, some minor fixes and comments minor improve:	SMSConsumer	- added some comments

--- tc65lib/src/org/javacint/sms/SMSConsumer.java
+++ tc65lib/src/org/javacint/sms/SMSConsumer.java
@@ -5,10 +5,16 @@
 package org.javacint.sms;
 
 /**
- *
- * @author florent
+ * Base SMS Consumer interface, used to respond to incoming SMSs
  */
 public interface SMSConsumer {
 
+    /**
+     * Method called by SMS receiver.
+     * Each consumer should return true once they consider it is of no other consumer's interest. It works the same with command receivers.
+     * @param from address of the sender
+     * @param content decoded text from SMS body
+     * @return true once the consumer consider it is of no other consumer's interest, false otherwise
+     */
     public boolean smsReceived(String from, String content);
 }
