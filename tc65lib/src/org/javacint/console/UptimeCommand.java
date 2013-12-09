7072d34eae6230fcd60446a0eb60ed6755c5a15e
Florent Clairambault <florent@clairambault.fr>
2013-12-10 00:06:27 +0100

    Uptime: Adding a frequent call back code to visualize when the uptime stops.

--- tc65lib/src/org/javacint/console/UptimeCommand.java
+++ tc65lib/src/org/javacint/console/UptimeCommand.java
@@ -2,23 +2,38 @@ package org.javacint.console;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.TimerTask;
+import org.javacint.task.Timers;
 
 public class UptimeCommand implements ConsoleCommand {
 
     private static final long startup = System.currentTimeMillis();
 
-    public boolean consoleCommand(String command, InputStream is, PrintStream out) {
+    public boolean consoleCommand(String command, InputStream is, final PrintStream out) {
         if (command.equals("uptime")) {
-            long s = (System.currentTimeMillis() - startup) / 1000;
-            long m = s / 60;
-            s -= (60 * m);
-            long h = m / 60;
-            m -= (60 * h);
-            out.println("[UPTIME] " + h + "h " + m + "m " + s + "s");
+            showUptime(out);
+            return true;
+        } else if (command.startsWith("uptime ")) {
+            int time = Integer.parseInt(command.substring("uptime ".length()));
+            Timers.getSlow().schedule(new TimerTask() {
+                public void run() {
+                    showUptime(out);
+                }
+            }, 0, time * 1000);
             return true;
         } else if (command.equals("help")) {
             out.println("[HELP] uptime - Show the program's uptime");
+            out.println("[HELP] uptime <s> - Show the program's frequently");
         }
         return false;
+    }
+
+    private void showUptime(PrintStream out) {
+        long s = (System.currentTimeMillis() - startup) / 1000;
+        long m = s / 60;
+        s -= (60 * m);
+        long h = m / 60;
+        m -= (60 * h);
+        out.println("[UPTIME] " + h + "h " + m + "m " + s + "s");
     }
 }
