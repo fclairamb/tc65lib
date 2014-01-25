36b186390347615cc3dcac76c8fb14e58c8e6fb5
Florent Clairambault <florent@clairambault.fr>
2014-01-25 20:01:39 +0100

    Time is in seconds. We might change that in the future.
    Please not that if you need to fetch the time frequently, you should use the jvm's System.currentTimeMillis().

--- tc65lib/src/org/javacint/time/HttpHeaderTimeClient.java
+++ tc65lib/src/org/javacint/time/HttpHeaderTimeClient.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.javacint.time;
 
 import java.io.IOException;
@@ -11,17 +7,21 @@ import javax.microedition.io.HttpConnection;
 /**
  * Simple HTTP Header time client.
  *
- *Gets time (GMT) from HTTP header.
+ * Gets time (GMT) from the "Date" HTTP header by performing a HEAD request.
  */
 public class HttpHeaderTimeClient implements TimeClient {
 
     private final String url;
 
+    /**
+     * Constructor.
+     *
+     * @param url URL to get the time from.
+     */
     public HttpHeaderTimeClient(String url) {
         this.url = url;
     }
 
-    
     public long getTime() throws IOException {
         HttpConnection conn = null;
         try {
@@ -29,7 +29,7 @@ public class HttpHeaderTimeClient implements TimeClient {
             conn.setRequestMethod(HttpConnection.HEAD);
             long time = conn.getDate();
             if (time != 0) {
-                return time;
+                return time / 1000;
             } else {
                 throw new RuntimeException("Server returned no valid date in http header");
             }
