1096b9284da4320039c572f19b036aa728afdde7
Florent Clairambault <florent@clairambault.fr>
2014-02-02 12:36:15 +0100

    Added an ADC check task.

---
+++ tc65lib/src/org/javacint/intsens/ADCCheckTask.java
@@ -0,0 +1,46 @@
+package org.javacint.intsens;
+
+import com.siemens.icm.io.ADC;
+import java.util.TimerTask;
+import org.javacint.logging.Logger;
+
+public abstract class ADCCheckTask extends TimerTask {
+
+    private final int adcNb;
+    private final int diff;
+    private int lastValue = -1000;
+    private ADC adc;
+
+    public ADCCheckTask(int adcNb, int diff) {
+        this.adcNb = adcNb;
+        this.diff = diff;
+    }
+
+    public void run() {
+        try {
+            if (lastValue == -1000) {
+                adc = new ADC(adcNb, 0);
+            }
+
+            int value = adc.getValue();
+
+            if (Math.abs(value - lastValue) >= diff) {
+                lastValue = value;
+                changed(value);
+            }
+        } catch (Exception ex) {
+            if (Logger.BUILD_CRITICAL) {
+                Logger.log(this + ".run", ex, true);
+            }
+
+            // If we fail, it's best that we don't run anymore
+            cancel();
+        }
+    }
+
+    public String toString() {
+        return "ADCCheckTask";
+    }
+
+    public abstract void changed(int temp);
+}
